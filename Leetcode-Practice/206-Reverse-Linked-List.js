a7fdf42f1cc3a0794b9ca76c4cc9f91d34bad0e0
Aninda Mukherjee <aninda_mukherjee@hotmail.com>
2020-07-25 00:45:47 -0700

    adding 206 Leetcode problem

--- Leetcode-Practice/206-Reverse-Linked-List.js
+++ Leetcode-Practice/206-Reverse-Linked-List.js
@@ -3,6 +3,27 @@
 // Input: 1->2->3->4->5->NULL
 // Output: 5->4->3->2->1->NULL
 
-function reverseList(head) {
-    
+function reverseList(ListNode) {
+
+    let current = ListNode;
+    if (!current) {
+        return current;
+    }
+
+    let currentPre = ListNode.next;
+    current.next = null;
+
+    while (currentPre) {
+        let temp = currentPre.next;
+        curPre.next = current;
+        current = currentPre;
+        currentPre = temp;
+    }
+
+   
+
+
+    return true;
 };
+
+console.log(reverseList([1,2,3,4,5]));
